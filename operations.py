c47fc55d432ab2ef15ba89217176e07930eb31b5
Ankit kumar bindu <ank.kumar077@gmail.com>
2023-08-18 00:43:21 +0545

    update

--- operations.py
+++ operations.py
@@ -1,8 +1,6 @@
 from read import *
 from write import *
-
-
-
+from datetime import datetime
 
 def welcome_message():
     print("\n")
@@ -19,70 +17,119 @@ def footer_component():
     print("\n")
     print("\t \t \t \t \t \t                Thank you.")
     print("\n")
+
+def user_info():
+    print("\n")
+   
     
-
-
-def options():
-    loop = True
-    while loop == True:
-        userInput = (input("\t \t \t \t \t    Choose the operation you want to continue: "))
-        if userInput == "1":
-            rent_item()
-            
-        elif userInput == "2":
-            return_item()
+    while True:
+        try:
+            name_of_user = input("\t \t \t \t \t    Enter your name: ")
+            if not name_of_user.isalpha():
+                raise ValueError("Name can only contain alphabetic characters.")
             break
-        elif userInput == "3":
-            exit_program()
+        except ValueError as e:
+            print(e)
+    
+    while True:
+        try:
+            phone_num_of_user = input("\t \t \t \t \t    Enter your phone number: ")
+            if not phone_num_of_user.isdigit() or len(phone_num_of_user) != 10:
+                raise ValueError("Phone number should be numeric and have 10 digits.")
             break
-        else: 
-            print("Enter valid input")
-            
-            
-def rent_item():
-        print("\n")
-        print("\t \t \t \t \t           # You have Choosen Rent option.")
-        name_of_user = input("\t \t \t \t \t    Enter your name: ")
-        phone_num_of_user = input("\t \t \t \t \t    Enter your phone number: ")
-        print("Thank you for your name "  + name_of_user.upper())
+        except ValueError as e:
+            print(e)
+    
+    print("Thank you for your Name and Phone Number " + name_of_user.upper())
+    
+    return name_of_user, phone_num_of_user
+
+def print_item_list():
+    rent_more_item = True
+    while rent_more_item == True:
         print("Given below are the list of item can be rented. ")
         print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
         print("S.N. \t\t  Item Name     \t\t\t      Company Name  \t\t\t       Price \t\t\t             Quantity")
         print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")   
         show_item()
         print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")   
-        rent_validation()
-        
+        rent_more_item = False    
 
+def rent_validation():
+    myDictionary = store_in_dictionary()
+    item_rented_by_user = []
+   
+    while True:
+        valid_id = input("Enter the id of the item you want to rent (or 'exit' to finish): ")
+            
+        try:
+            if valid_id.lower() == 'exit':
+                print("\n")
+                print("\t \t \t \t \t    #Enter you Name and Phone Number to Print your bill")
+                break
+            
+            valid_id = int(valid_id)
+            if valid_id < 0 or valid_id >= len(myDictionary):
+                print("Enter a correct or valid id.")
+                continue
+            
+            while True:
+                try:
+                    quantity = int(input("Enter the quantity of the item you want to rent: "))
+                    get_quantity_of_selected_item = int(myDictionary[valid_id][3])
 
+                    if quantity <= 0 or quantity > get_quantity_of_selected_item:
+                        print("The item's quantity you want to rent doesn't appear to be available in the store.")
+                    else:
+                        break  # Exit the quantity input loop if valid
+                except ValueError:
+                    print("Invalid input. Please enter a valid integer.")
+                    
+            updated_quantity = int(get_quantity_of_selected_item) - int(quantity)
+            myDictionary[valid_id][3] = str(updated_quantity)
+            update_item_quantity(myDictionary)
+            
+            item_name = myDictionary[valid_id][0]
+            user_selected_quantity = quantity
+            item_price = myDictionary[valid_id][2]
+            selected_item_price = item_price.replace("$", "")
+            total_bill = int(selected_item_price) * int(quantity)
 
+            item_rented_by_user.append([item_name, user_selected_quantity, item_price, total_bill])
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+
+    today_date_and_time = datetime.now()
+    name_of_user, phone_num_of_user = user_info()
+    print("\n")
+    print("\t \t \t \t \t \t  Welcome to Event Equipment Rental Shop")
+    print("\t \t \t \t \t \t             Kathmandu, Nepal")
+    print("\n")
+    print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("\t \t \t \t \t \t             Customer Details")
+    print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("Name of the customer:", str(name_of_user).upper())
+    print("Contact number:", str(phone_num_of_user))
+    print("Date and time of renting:", str(today_date_and_time))
+    print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("\t \t \t \t \t \t             Items Details")
+    print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+    print("Item Name \t\t\t\t\t  Total Quantity \t     Unit Price \t\t\t\t\tTotal")
+    print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    total = 0
+    for i in item_rented_by_user:
+        print(i[0], "\t\t\t", i[1], "\t\t\t", str(i[2]), "\t\t\t", "${}".format(i[3]))
+        total += int(i[3])
+    print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("\t\t\t\t\t\t\t\t\t\t\t\t\t            Grand Total: ${}".format(total))
+    print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("# Duration of Rented Item is for 5 Days.")
+    print("Note: In case of Delay, you will be Fined.")  
+    print("\n")
 def return_item():
     print("Thank you for returning")  
-    
-
 
 def exit_program():
     print("Thank you for using our system")  
 
-
-def rent_validation():
-    
-    valid_id = int(input("Enter the id of the item you want to rent."))
-    myDictionary = store_in_dictionary()
-    
-    while valid_id < 0 or valid_id > len(myDictionary):
-       print("Enter correct or valid id") 
-       valid_id = int(input("Enter the id of the item you want to rent."))
-    
-    quantity = int(input("Enter the quantity of the item you want to rent."))
-    
-    get_quantity_of_selected_item = myDictionary[valid_id][3]
-    while quantity <= 0 or quantity > int(get_quantity_of_selected_item):
-        print("The item's quantity you want to rent doesn't appears to available in the store.")
-        print("\n")
-        quantity = int(input("Enter the quantity of the item you want to rent."))
-    print("\n")
-    
-    updated_quntity= int(get_quantity_of_selected_item) - int(quantity)
-    myDictionary[valid_id][3] = str(updated_quntity)
-    update_item_quantity(myDictionary)
--- write.py
+++ write.py
@@ -4,7 +4,7 @@ def update_item_quantity(updated_dictionary):
     
     with open("items-info.txt", "w") as file:
         for values in updated_dictionary.values():
-            file.write(",".join(values))
+            file.write(str(values[0]) + "," + str(values[1]) + ","+ str(values[2]) + ","+ str(values[3]) + "," )
             file.write("\n")
         
     
